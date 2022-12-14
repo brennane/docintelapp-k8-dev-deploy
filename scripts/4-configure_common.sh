dc0ce488884a57431b93e44101fa7b3cd6f20855
Brennan Evans <breevans@cisco.com>
2022-12-14 10:31:43 -0600

    updates

--- scripts/4-configure_common.sh
+++ scripts/4-configure_common.sh
@@ -8,9 +8,7 @@ sh scripts/_configure_postgres.sh
 
 echo ""
 echo "=== CONFIGURING SOLR (docintel ${branch} solrCloud ${solr_zookeeper})"
-#sh scripts/_make_solr_configsets_prep.sh ${branch}
-#tar cf - solr_configsets | kubectl exec -i -n docintel --container solr solr-0 -- tar xf - -C /tmp
-kubectl -n docintel cp scripts/_make_solr_configsets_old.sh  --container solr solr-0:/tmp/make_solr_configsets.sh
+kubectl -n docintel cp scripts/_make_solr_configsets_remote.sh  --container solr solr-0:/tmp/make_solr_configsets.sh
 kubectl exec -i -n docintel --container solr solr-0 -- sh /tmp/make_solr_configsets.sh $branch $solr_zookeeper create
 
 echo ""
